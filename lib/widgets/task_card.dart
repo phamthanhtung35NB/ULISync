ae27fed7cc7e29d588775fae002119270a7c2e6f
Phạm Thanh Tùng <tungpham010203@gmail.com>
2024-08-28 16:52:20 +0700

    set color

--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -7,42 +7,69 @@ class TaskCard extends StatelessWidget {
   TaskCard({required this.task});
   Color _getTaskColor(Task task) {
     print(task.status);
-    if (task.status == 'đã hoàn thành') {
-      return Colors.green;
+    if (task.status == 'Đã hoàn thành') {
+      return Color(0xFFB4F6A4);
     // } else if (task.deadline.isBefore(DateTime.now())) {
-    } else if (task.status == 'Thiếu') {
-      return Colors.red;
+    } else if (task.status == 'Quá hạn') {
+      //A93932FF
+      return Color(0xFFF898A4);
 
+    } else if (task.status == 'Đang tiến hành') {
+      return Color(0xFFF7FAA1);
     } else {
-      return Colors.yellow;
+      return Colors.blue;
     }
   }
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      color: _getTaskColor(task),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        // color: _getTaskColor(task),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              task.title,
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+@override
+Widget build(BuildContext context) {
+  return Stack(
+    children: [
+      Positioned(
+        child: Padding(
+          padding: const EdgeInsets.only(right: 19.0,left: 5),
+          child: ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10.0),
+              bottomLeft: Radius.circular(10.0),
+              topRight: Radius.circular(10.0),
             ),
-            SizedBox(height: 8.0),
-            Text(task.description),
-            SizedBox(height: 8.0),
-            Text('Status: ${task.status}'),
-            SizedBox(height: 8.0),
-            Text('Deadline: ${task.deadline}'),
-            SizedBox(height: 8.0),
-            Text('Submission Date: ${task.timeSubmitted}'),
-          ],
+            child: Container(
+              width: double.infinity - 8,
+              //nhỏ hơn 10
+              height:  177,
+              color: _getTaskColor(task),
+            ),
+          ),
         ),
       ),
-    );
-  }
+      Container(
+        width: double.infinity,
+        child: Card(
+          margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 8.0),
+                Text(
+                  task.title,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8.0),
+                Text(task.description),
+                SizedBox(height: 8.0),
+                Text('Status: ${task.status}'),
+                SizedBox(height: 8.0),
+                Text('Deadline: ${task.deadline}'),
+                SizedBox(height: 8.0),
+                Text('Submission Date: ${task.timeSubmitted}'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
+}
 }
